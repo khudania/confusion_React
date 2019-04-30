46f8a63e3c86df50d2ba7513643cee8953f5126f
hkhudania <hkhudania@gmail.com>
2019-04-30 12:06:59 +0530

    configuring React

--- src/App.js
+++ src/App.js
@@ -13,12 +13,11 @@ class App extends Component {
   render() {
     return ( 
       <div className = "App" >
-      <Navbar dark color = "primary" >
-      <div className = "container" >
-      <NavbarBrand href = "/" > Ristorante Con Fusion 
-      </NavbarBrand>  
-      </div > 
-      </Navbar>  
+        <Navbar dark color = "primary" >
+          <div className = "container" >
+            <NavbarBrand href = "/" > Ristorante Con Fusion </NavbarBrand>  
+          </div > 
+        </Navbar>  
       </div >
     );
   }
